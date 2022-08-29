33160e2b86d44b17bf4dd4237b5e33fbf85364ce
Tiago Rocha <tiagolopesrocha@tecnico.ulisboa.pt>
2022-08-29 21:11:56 +0100

    Fix naming issue

--- src/libpfm4/lib/pfmlib_riscv_priv.h
+++ src/libpfm4/lib/pfmlib_riscv_priv.h
@@ -11,6 +11,7 @@ typedef struct
     const char *desc;  /* event description */
 } riscv_entry_t;
 
-extern int pfm_riscv_detect(void *this);
 
-#endif /* __PFMLIB_RIScV_PRIV_H__ */
+extern int pfm_riscv_detect_sifive_u74(void *this);
+
+#endif /* __PFMLIB_RISCV_PRIV_H__ */
