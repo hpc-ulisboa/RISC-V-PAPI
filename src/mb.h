hpc-ulisboa/RISC-V-PAPI

commit f1d034b768735f8e09bc88ba3e34bc4133adeecf

Fix Error. Order of else if and else was switched

src/mb.h
@@ -59,12 +59,13 @@
 #if defined(__KNC__)
 #define rmb() __sync_synchronize()
 
+#elif defined(__riscv)
+#define rmb() asm volatile("fence":::"memory")
+
 #else
 #define rmb() asm volatile("lfence":::"memory")
 #endif
 
-#elif defined(__riscv)
-#define rmb() asm volatile("fence":::"memory")
 
 #else
 #error Need to define rmb for this architecture!

commit 359e4a7970a923fc89be47b310a72d1367a1ff7b

Fix error introduced in commit f1d034b768735f8e09bc88ba3e34bc4133adeecf.
RISC-V case was wrongly moved into a nested if and would never be activated.
Prevents correct compilation.

src/mb.h
@@ -58,14 +58,12 @@
 
 #if defined(__KNC__)
 #define rmb() __sync_synchronize()
-
-#elif defined(__riscv)
-#define rmb() asm volatile("fence":::"memory")
-
 #else
 #define rmb() asm volatile("lfence":::"memory")
 #endif
 
+#elif defined(__riscv)
+#define rmb() asm volatile("fence":::"memory")
 
 #else
 #error Need to define rmb for this architecture!