8132e72d6c741ce14f00285bcff5979bcc340d2a
Tiago Rocha <tiagolopesrocha@tecnico.ulisboa.pt>
2023-02-21 22:18:30 +0000

    Add all tests to script

--- sifive_u74_tests/tests.sh
+++ sifive_u74_tests/tests.sh
@@ -2,8 +2,16 @@
 # Compile test files
 echo $(make)
 TIMEFORMAT=%R
+
+echo "no_count_no_time - 20 executions" >> $1
+for ((i=0; i<20;i++))
+do
+    time {
+        echo $(./no_count_no_time) &>> $1
+    } &>> $1
+done
+
 echo "Perf_no_time - 20 executions" >> $1
-# Do 20 executions of perf_no_time test
 for ((i=0; i<20;i++))
 do
     time {
@@ -19,5 +27,28 @@ do
     } &>> $1
 done
 
-echo $(unset TIMEFORMAT)
+echo "no_count_time - 20 executions" >> $1
+for ((i=0; i<20;i++))
+do
+    time {
+        echo $(./no_count_time) &>> $1
+    } &>> $1
+done
 
+echo "Perf_time - 20 executions" >> $1
+for ((i=0; i<20;i++))
+do
+    time {
+        echo $(./perf_time) &>> $1
+    } &>> $1
+done
+
+echo "PAPI_time - 20 executions" >> $1
+for ((i=0; i<20;i++))
+do
+    time {
+        echo $(./papi_time) &>> $1
+    } &>> $1
+done
+
+echo $(unset TIMEFORMAT)
