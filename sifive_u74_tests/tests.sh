d84c68dd545353c3a509e8e81d9d0a906a0f168a
Tiago Rocha <tiagolopesrocha@tecnico.ulisboa.pt>
2023-02-21 22:29:40 +0000

    Change number of tests to 40 each

--- sifive_u74_tests/tests.sh
+++ sifive_u74_tests/tests.sh
@@ -3,48 +3,48 @@
 echo $(make)
 TIMEFORMAT=%R
 
-echo "no_count_no_time - 20 executions" >> $1
-for ((i=0; i<20;i++))
+echo "no_count_no_time - 40 executions" >> $1
+for ((i=0; i<40;i++))
 do
     time {
         echo $(time ./no_count_no_time) &>> $1
     } &>> $1
 done
 
-echo "Perf_no_time - 20 executions" >> $1
-for ((i=0; i<20;i++))
+echo "Perf_no_time - 40 executions" >> $1
+for ((i=0; i<40;i++))
 do
     time {
         echo $(time ./perf_no_time) &>> $1
     } &>> $1
 done
 
-echo "PAPI_no_time - 20 executions" >> $1
-for ((i=0; i<20;i++))
+echo "PAPI_no_time - 40 executions" >> $1
+for ((i=0; i<40;i++))
 do
     time {
         echo $(time ./papi_no_time) &>> $1
     } &>> $1
 done
 
-echo "no_count_time - 20 executions" >> $1
-for ((i=0; i<20;i++))
+echo "no_count_time - 40 executions" >> $1
+for ((i=0; i<40;i++))
 do
     time {
         echo $(time ./no_count_time) &>> $1
     } &>> $1
 done
 
-echo "Perf_time - 20 executions" >> $1
-for ((i=0; i<20;i++))
+echo "Perf_time - 40 executions" >> $1
+for ((i=0; i<40;i++))
 do
     time {
         echo $(time ./perf_time) &>> $1
     } &>> $1
 done
 
-echo "PAPI_time - 20 executions" >> $1
-for ((i=0; i<20;i++))
+echo "PAPI_time - 40 executions" >> $1
+for ((i=0; i<40;i++))
 do
     time {
         echo $(time ./papi_time) &>> $1
